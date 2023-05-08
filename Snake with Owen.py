e0aff48018f99aac508961d64afcb5b0b24d70c6
Eshwar Muneshar <Emuneshar@gmail.com>
2023-05-08 17:25:19 -0400

    Added code for the food

--- Snake with Owen.py
+++ Snake with Owen.py
@@ -12,5 +12,19 @@ head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
 head.color("blue")
+head.penup()
+head.goto(0,0)
+
+# Food code
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
 
 
+while True:
+    window.update()
+
+    time.sleep(0.1)
