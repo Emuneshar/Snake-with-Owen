35dd580e6237ecc600553a460f1cbfe391773c44
Eshwar Muneshar <Emuneshar@gmail.com>
2023-05-08 17:06:03 -0400

    Added head code

--- Snake with Owen.py
+++ Snake with Owen.py
@@ -7,3 +7,10 @@ window.title("Snake Game")
 window.bgcolor("black")
 window.setup(width = 600, height = 600)
 window.tracer(0)
+
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("blue")
+
+
